17ad3f55cca43576dadb1a0121bc34ea28a418de
Agrael11 <Oliver.Neuschl@gmail.com>
2023-08-11 16:05:48 +0200

    Updated Window Name

--- src/Main.cpp
+++ src/Main.cpp
@@ -108,7 +108,7 @@ int main(int argc, char *args[])
     Logger::SaveTimedCopy = false;
     Logger::OverrideFile = true;
 
-    Game tempGame = Game(512, 512, "Test Window");
+    Game tempGame = Game(512, 512, "LRJ_23");
 
     game = &tempGame;
     game->Init();
